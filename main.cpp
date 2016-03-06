8265325d2e868503c613e41651dfb63d39960d6c
Dominic Reich <dominic@mm.st>
2016-03-06 22:32:41 +0100

    Adds license text in main.cpp, adds vim modeline

--- main.cpp
+++ main.cpp
@@ -4,9 +4,29 @@
  * 
  * Author: Dominic Reich <dominic@tmsn.at>
  * Created on 20. Februar 2016, 16:11
- * Last modified: Samstag, 20.02.2016 20:02
+ * Last modified: Freitag, 04.03.2016 23:39
  * 
- * This file is licensed under the terms of the MIT license
+ * The MIT License (MIT) {{{
+ * 
+ * Copyright (c) 2015 Dominic Reich
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. }}}
  * 
  */
 
@@ -448,3 +468,6 @@ void ToggleEntry( const char *Filename, const char *Counter ) {
     cout << "Sorry, diese Funktion ist noch in Arbeit." << endl;
     PressKeyToContinue();
 }
+
+/* vim: set ts=2 sw=2 tw=0 et :*/
+
